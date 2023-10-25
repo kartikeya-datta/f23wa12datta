e939c976099dd6babfff0a1a434e69750968eab3
M KARTIKEYA DATTA <kartikeyadatta3@gmail.com>
2023-10-25 17:32:37 -0500

    updated users.js with add and more variables

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more+=1;
+  sum+=more;
+  res.send(`Sum is: ${sum}`);
 });
 
 module.exports = router;
+let sum= 0;
+let more= 0;
