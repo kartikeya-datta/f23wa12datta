af05e8bbcc69c05b6f60dccaaac0c8aef28dc307
M KARTIKEYA DATTA <kartikeyadatta3@gmail.com>
2023-10-25 21:45:49 -0500

    added next line in computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -7,10 +7,10 @@ router.get('/', function(req, res, next) {
     router.put( y = Math.round(Math.random()*10));
     router.put( z = Math.round(Math.random()));
     router.post(p = Math.pow(x, y));
-    router.post(s = Math.sign(x));
+    router.post(s = Math.sign(x)); 
     router.post(ta = Math.tanh(z));
     router.post(tr = Math.trunc(y));
-  res.send(`power applied to ${x} and ${y} is ${p}\n sign applied to ${x} is ${s}\n tanh applied to ${z} is ${ta}\n trunc applied to ${y} is ${tr}`)
+    res.send(`power applied to ${x} and ${y} is ${p}`+\n+`sign applied to ${x} is ${s}`+\n+`tanh applied to ${z} is ${ta}`+\n+`trunc applied to ${y} is ${tr}`)
 });
 
 module.exports = router;
