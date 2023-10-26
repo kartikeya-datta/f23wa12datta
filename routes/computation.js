ed520551b3267aeffd56e64d3fc3633f8221a54f
M KARTIKEYA DATTA <kartikeyadatta3@gmail.com>
2023-10-25 20:28:29 -0500

    made changes in computation

--- routes/computation.js
+++ routes/computation.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('computation', { title1: `Kartikeya's Bonus` });
     router.put( x = Math.round(Math.random()*10));
     router.put( y = Math.round(Math.random()*10));
     router.put( z = Math.round(Math.random()));
