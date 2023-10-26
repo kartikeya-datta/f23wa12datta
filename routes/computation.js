377da86ee5e52af7d2db867cfcd82b75f8d3d1ad
M KARTIKEYA DATTA <kartikeyadatta3@gmail.com>
2023-10-25 19:26:03 -0500

    Created a new computation.js file for math functions

---
+++ routes/computation.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('computation', { title: `Kartikeya's Bonus` });
+    router.put( x = Math.round(Math.random()*10));
+    router.put( y = Math.round(Math.random()*10));
+    router.post(p = Math.pow(x, y));
+    router.post(s = Math.sign(x));
+    router.post(tanh(x/10));
+    router.post(trunc(y));
+  res.render('index', { title: 'Express' });
+  res.send(`power applied to ${x} and ${y} is ${p}\n sign applied to ${x} is ${s}\n tanh applied to ${x/10} is ${ta}\n trunc applied to ${y} is ${tr}`)
+});
+
+module.exports = router;
+let x;
+let y;
+let p;
+let s;
+let ta;
+let tr;
