a081c9840d85798ce1be5064f0fbb22df9bade0b
M KARTIKEYA DATTA <kartikeyadatta3@gmail.com>
2023-10-25 19:49:08 -0500

    made modifications in computation file

--- routes/computation.js
+++ routes/computation.js
@@ -3,19 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('computation', { title: `Kartikeya's Bonus` });
+    res.render('computation', { title1: `Kartikeya's Bonus` });
     router.put( x = Math.round(Math.random()*10));
     router.put( y = Math.round(Math.random()*10));
+    router.put( z = Math.round(Math.random()));
     router.post(p = Math.pow(x, y));
     router.post(s = Math.sign(x));
-    router.post(ta = tanh(x/10));
+    router.post(ta = tanh(z));
     router.post(tr = trunc(y));
-  res.send(`power applied to ${x} and ${y} is ${p}\n sign applied to ${x} is ${s}\n tanh applied to ${x/10} is ${ta}\n trunc applied to ${y} is ${tr}`)
+  res.send(`power applied to ${x} and ${y} is ${p}\n sign applied to ${x} is ${s}\n tanh applied to ${z} is ${ta}\n trunc applied to ${y} is ${tr}`)
 });
 
 module.exports = router;
 let x;
 let y;
+let z;
 let p;
 let s;
 let ta;
